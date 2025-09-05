460177aebd1c137a2e2debb8ee9f1e282198eff1
Norrin Radd <bluebeardblackheart@gmail.com>
2025-09-05 09:16:25 -0500

    Update ip.sh
    added some more links

--- ip_sh/ip.sh
+++ ip_sh/ip.sh
@@ -4,26 +4,26 @@ green() { echo -e "\033[32m$1\033[0m"; }
 yellow() { echo -e "\033[33m$1\033[0m"; }
 red() { echo -e "\033[31m$1\033[0m"; }
 
-ip1=$(curl -s http://ipinfo.io/ip)
-echo $ip1
-ip2=$(curl -s http://ifconfig.me)
-echo $ip2
-ip3=$(curl -s http://checkip.amazonaws.com)
-echo $ip3
-ip4=$(curl -s http://icanhazip.com)
-echo $ip4
-ip5=$(curl -s http://whatismyip.akamai.com)
-echo $ip5
-ip6=$(curl -s http://ident.me)
-echo $ip6
-ip7=$(curl -s http://myexternalip.com/raw)
-echo $ip7 
-ip8=$(curl -s https://api.ipify.org)
-echo $ip8
-ip10=$(curl -s http://ipecho.net/plain)
-echo $ip10
+ip1=$(curl -s https://api.ipify.org)                # Cloudflare (via ipify.org)
+ip2=$(curl -s https://ifconfig.me)                  # ifconfig.me
+ip3=$(curl -s https://ipinfo.io/ip)                 # ipinfo.io
+ip4=$(curl -s https://checkip.amazonaws.com)        # AWS
+ip5=$(curl -s https://icanhazip.com)                # icanhazip.com
+ip6=$(curl -s https://whatismyip.akamai.com)        # Akamai
+ip7=$(curl -s https://ident.me)                     # ident.me
+ip8=$(curl -s https://myexternalip.com/raw)         # myexternalip.com
+ip9=$(curl -s https://ipecho.net/plain)             # ipecho.net
+ip10=$(curl -s https://ipv4.icanhazip.com)          # Forces IPv4
+ip11=$(curl -s https://ipv6.icanhazip.com)          # Forces IPv6
+ip12=$(curl -s https://wtfismyip.com/text)          # wtfismyip.com
+ip13=$(curl -s https://ip.42.pl/raw)                # Polish simple IP service
+ip14=$(curl -s https://ip.seeip.org)                # seeip.org
+ip15=$(curl -s https://ipecho.io/plain)             # ipecho.io
+ip16=$(curl -s https://trackip.net/ip)              # trackip.net
+ip17=$(curl -s https://ipwho.is/?format=raw | grep ip | cut -d\" -f4) # ipwho.is
+ip18=$(curl -s https://get.geojs.io/v1/ip)          # geojs.io
 
-ips=("$ip1" "$ip2" "$ip3" "$ip4" "$ip5" "$ip6" "$ip7" "$ip8" "$ip10")
+ips=("$ip1" "$ip2" "$ip3" "$ip4" "$ip5" "$ip6" "$ip7" "$ip8" "$ip9"  "$ip9"  "$ip10"  "$ip11"  "$ip12"  "$ip13"  "$ip14"  "$ip15"  "$ip16"  "$ip17"  "$ip18")
 
 declare -A cnt
 for ip in "${ips[@]}"; do
